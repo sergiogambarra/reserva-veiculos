b0f2398d58d32e74e50aa9e5b7e5979fa47e9f9b
erpeck@restinga.ifrs.edu.br <erpeck@restinga.ifrs.edu.br@32a6413f-c3f1-e00a-3fa0-47ab75e0a58c>
2014-04-21 02:37:38 +0000

    atualizacao do banco

--- trunk/SRV/Documentos/03 - Implementação/SRV-Banco de dados/SRV-Script Banco de dados.sql
+++ trunk/SRV/Documentos/03 - Implementação/SRV-Banco de dados/SRV-Script Banco de dados.sql
@@ -2,14 +2,14 @@ CREATE SCHEMA IF NOT EXISTS SRV;
 
 USE SRV;
 
-CREATE TABLE IF NOT EXISTS funcionario(
+CREATE TABLE IF NOT EXISTS servidor(
 matricula_siape VARCHAR(10),
 senha VARCHAR(10),
 perfil TINYINT(1),
 email VARCHAR(50),
 PRIMARY KEY (matricula_siape));
 
-insert into funcionario (matricula_siape, senha, perfil, email)
+insert into servidor (matricula_siape, senha, perfil, email)
 values ('admin', '1234', '1', 'erpeck@restinga.ifrs.edu.br'), 
 ('serv', '5678', '0', 'erpeck@restinga.ifrs.edu.br');
 
