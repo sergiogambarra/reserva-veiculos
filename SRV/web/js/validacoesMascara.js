2d52220f636d4bb043a0839d03c01e044f07cc7f
lmendes <lmendes@restinga.ifrs.edu.br>
2014-06-09 19:19:10 +0000

    validações atualizadas (falta telefone, placa e data_man).

--- SRV/web/js/validacoesMascara.js
+++ SRV/web/js/validacoesMascara.js
@@ -1,5 +1,4 @@
 function validarMatSiap(mat){
-	
 	if ((mat.length <= 0) || (mat.length !== 7) || (isNaN(mat))){
 		alert("Matricula inválida. Digite 7 números.");
 		return false;
@@ -13,78 +12,22 @@ function mascaraMatSiap(e){
 	if((digito > 47) && (digito < 58)){
 		return true;
 	}else{
-		return false;
-	}
+	if (digito != 8) // backspace  
+                //event.keyCode = 0;  
+                return false;  
+            else  
+                return true;  
+        }  
 }
 
-//não funcionando
-function mascaraLetras(){
-	    tecla = event.keyCode;  
-    if (tecla >= 33 && tecla <= 64 || tecla >= 91 && tecla <= 93 || tecla >= 123 && tecla <= 159 || tecla >= 162 && tecla <= 191 ){   
-        return false;  
-    }else{  
-       return true;  
-    }  
-}  
-
-//adiciona mascara de data
-function MascaraData(data){
-	if(mascaraInteiro(data)==false){
-		event.returnValue = false;
-	}	
-	return formataCampo(data, '00/00/0000', event);
-}
-
-//adiciona mascara ao telefone
-function MascaraTelefone(tel){	
-	if(mascaraInteiro(tel)==false){
-		event.returnValue = false;
-	}	
-	return formataCampo(tel, '(00) 0000-0000', event);
-}
-
-//adiciona mascara ao CPF
-function MascaraCPF(cpf){
-	if(mascaraInteiro(cpf)==false){
-		event.returnValue = false;
-	}	
-	return formataCampo(cpf, '000.000.000-00', event);
-}
-
-//valida telefone
-function ValidaTelefone(tel){
-	exp = /\(\d{2}\)\ \d{4}\-\d{4}/
-	if(!exp.test(tel.value))
-		alert('Numero de Telefone Invalido!');
-}
-
-//valida data
-function ValidaData(data){
-	exp = /\d{2}\/\d{2}\/\d{4}/
-	if(!exp.test(data.value))
-		alert('Data Invalida!');			
-}
-
-//valida o CPF digitado
-function ValidarCPF(Objcpf){
-	var cpf = Objcpf.value;
-	exp = /\.|\-/g
-	cpf = cpf.toString().replace( exp, "" ); 
-	var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
-	var soma1=0, soma2=0;
-	var vlr =11;
-	
-	for(i=0;i<9;i++){
-		soma1+=eval(cpf.charAt(i)*(vlr-1));
-		soma2+=eval(cpf.charAt(i)*vlr);
-		vlr--;
-	}	
-	soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
-	soma2=(((soma2+(2*soma1))*10)%11);
-	
-	var digitoGerado=(soma1*10)+soma2;
-	if(digitoGerado!=digitoDigitado)	
-		alert('CPF Invalido!');		
+ 
+function mascaraLetras(e){
+    var dig = (window.event)?event.keycode:e.which;
+    if (dig >= 33 && dig <= 64 || dig >= 91 && dig <= 93 || dig >= 123 && dig <= 159 || dig >= 162 && dig <= 191 ){
+        return false;
+    }else{
+        return true;
+    }
 }
 
 //valida numero inteiro com mascara
@@ -94,39 +37,6 @@ function mascaraInteiro(){
 		return false;
 	}
 	return true;
-}
-
-//formata de forma generica os campos
-function formataCampo(campo, Mascara, evento) { 
-	var boleanoMascara; 
-	
-	var Digitato = evento.keyCode;
-	exp = /\-|\.|\/|\(|\)| /g
-	campoSoNumeros = campo.value.toString().replace( exp, "" ); 
-   
-	var posicaoCampo = 0;	 
-	var NovoValorCampo="";
-	var TamanhoMascara = campoSoNumeros.length;; 
-	
-	if (Digitato != 8) { // backspace 
-		for(i=0; i<= TamanhoMascara; i++) { 
-			boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
-								|| (Mascara.charAt(i) == "/")) 
-			boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
-								|| (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
-			if (boleanoMascara) { 
-				NovoValorCampo += Mascara.charAt(i); 
-				  TamanhoMascara++;
-			}else { 
-				NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
-				posicaoCampo++; 
-			  }	   	 
-		  }	 
-		campo.value = NovoValorCampo;
-		  return true; 
-	}else { 
-		return true; 
-	}
 }
 
 // Função que aplica uma máscara de formato ao Cpf
